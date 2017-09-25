80cea34b08d162673f7255294853bc0d24a616eb
synaplasticity <synaplasticity@gmail.com>
2017-09-25 15:18:29 +0530

    Added separate function for drawing the contour diagram

--- ML course/Week 2/machine-learning-ex1/ex1/gradientDescent_w_graph.m
+++ ML course/Week 2/machine-learning-ex1/ex1/gradientDescent_w_graph.m
@@ -3,66 +3,23 @@ function [theta, J_history] = gradientDescent_w_graph(X, y, theta, alpha, num_it
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
-format long;
+    format long;
 
-% Initialize some useful values
-m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
+    % Initialize some useful values
+    m = length(y); % number of training examples
+    J_history = zeros(num_iters, 1); % Compute cost per training data
 
-fprintf("\nFeature size is %f : ", m);
-
-%-----------------
-
-    % Grid over which we will calculate J
-    theta0_vals = linspace(-10, 10, 100);
-    theta1_vals = linspace(-1, 4, 100);
-
-    % initialize J_vals to a matrix of 0's
-    J_vals = zeros(length(theta0_vals), length(theta1_vals));
-
-    
-    % Fill out J_vals
-    for i = 1:length(theta0_vals)
-
-        for j = 1:length(theta1_vals)
-          t = [theta0_vals(i); theta1_vals(j)];
-          J_vals(i,j) = computeCost(X, y, t);
-
-          % fprintf("Theta =%0.15f",   J_vals(i,j));
-          
-        end
-    end
+    fprintf("\nFeature size is %f : ", m);
 
 
-    % Because of the way meshgrids work in the surf command, we need to
-    % transpose J_vals before calling surf, or else the axes will be flipped
-    J_vals = J_vals';
-    % Surface plot
-%     figure;
-%     surf(theta0_vals, theta1_vals, J_vals)
-%     xlabel('\theta_0'); ylabel('\theta_1');
-
-%     figure(9999);
-%     plot(X, y, 'kx', 'Markersize', 5);
-%     ylabel('Profit in $10,000s');
-%     xlabel('Population of City in 10,000s');
-
-    % Contour plot
-    figure(999);
-    % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-    % plot the starting points of theta
-    contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-    xlabel('\theta_0'); ylabel('\theta_1');
-    hold on;
-
-
-
-
-%-----------------
-
-
-
+    % Create the initial contour plot and the spot with initial
+    % theta values E.g. (0, 0)
+    drawInitialContour(X, y);
     plot(theta(1,:), theta(2,:), 'rx', 'MarkerSize', 20, 'LineWidth', 2);
+
+
+
+
 
     for iter = 1:num_iters
 
@@ -79,9 +36,6 @@ fprintf("\nFeature size is %f : ", m);
         x1_errDiffSum = 0;
         while (i <= m)
             x_i = X(i, :); % x(i). Row of X. E.g.: [1, 1.6]
-%             fprintf("\nTraining set %d, value %f,%f", i, x_i);
-    
-            %fprintf("\nx_i is %f and y_i is %f ", x_i, y(iter));
     
             hypoXi = x_i * theta; % % h_theta(x_i)
     
@@ -94,14 +48,13 @@ fprintf("\nFeature size is %f : ", m);
             i++;
         end
 
+        % Simulatenously update theta values
         theta(1,:) = ( theta(1,:) - (alpha * (1/m) * x0_errDiffSum) )  ; % final GD formula
         theta(2,:) = ( theta(2,:) - (alpha * (1/m) * x1_errDiffSum) )  ; % final GD formula
 
 
-
         % Save the cost J in every iteration    
-        cc = computeCost(X, y, theta);
-        J_history(iter) = cc;
+        J_history(iter) = computeCost(X, y, theta);
 
         % plot the predication line
         figure(9999);
