41cfbef9482ebe8885277662144f84d6e07650e3
synaplasticity <synaplasticity@gmail.com>
2017-10-16 15:45:19 +0530

    Refactored to use 2 dimensional vector

--- Spark/basic/src/test/scala/info/rewiring/spark/mllib/LinearRegressionTest.scala
+++ Spark/basic/src/test/scala/info/rewiring/spark/mllib/LinearRegressionTest.scala
@@ -14,8 +14,8 @@ class LinearRegressionTest extends FunSpec{
   val sc = new SparkContext(sparkConf)
   val data = sc.textFile("file:///opt/spark/data/mllib/popvsrev/ex1data1.csv")
 
-  val numOfIters = 75000
-  val learnRate = 0.09
+  val numOfIters = 50000
+  val learnRate = 0.099000000000009
 
   describe("Simple linear regression") {
 
@@ -32,7 +32,7 @@ class LinearRegressionTest extends FunSpec{
 
         assert(labeledPoint.count() === 97)
         assert(labeledPoint.first().label === 17.592 )
-        assert(labeledPoint.first().features === Vectors.dense(6.1101, 1))
+        assert(labeledPoint.first().features === Vectors.dense(1, 6.1101))
       }
 
       it("should correctly train the model") {
@@ -43,6 +43,7 @@ class LinearRegressionTest extends FunSpec{
 
 
         println("Weights ------> " + model.weights)
+        println("Intercept ------> " + model.intercept)
         linearRegression.evaluate(labeledPoint, model)
         assert(model.predict(Vectors.dense(1, 20.0)) === 17.14723903437474)
         assert(model.predict(Vectors.dense(1, 40.0)) === 34.29447806874948)
