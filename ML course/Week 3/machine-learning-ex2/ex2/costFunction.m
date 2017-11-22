395323682b522f631cadb1fa296e167f2937e9c8
synaplasticity <synaplasticity@gmail.com>
2017-11-22 16:02:09 +0530

    Sigmoid function and gradient

--- ML course/Week 3/machine-learning-ex2/ex2/costFunction.m
+++ ML course/Week 3/machine-learning-ex2/ex2/costFunction.m
@@ -20,8 +20,14 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
+% first transpose X for sigmoid function, so we can multiply (1x3)*(3x100) = (1x100).
+% Transpose y (100X1) to (1x100) before. We will have a (1x100) vector (step 2)
+%
+%   Now transpose X, so we get 3X100 vector
+%   Transpose step2 output so we get 100x1 vector
+%       Multiplication of these two parts should provide 3x1 vector -> size of
+%       theta
+grad = 1/m * ( X' * (sigmoid(-theta'*X') - y')' );
 
 
 
