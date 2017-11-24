7205e6ad9a645357bfcc482b8f67b908f0cb3fb7
synaplasticity <synaplasticity@gmail.com>
2017-11-24 11:55:29 +0530

    Re-formatted

--- ML course/Week 3/machine-learning-ex2/ex2/costFunctionReg.m
+++ ML course/Week 3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -4,47 +4,48 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
+    % Initialize some useful values
+    m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+    % You need to return the following variables correctly 
+    J = 0;
+    grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-% =============================================================
-
-
-%
-% Cost function J(theta)
-%
-% As theta0 is not regularized, we skip it
-cf_reg_expr = lambda/(2*m) * sum(theta([2 : size(theta)]) .^ 2);
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the cost of a particular choice of theta.
+    %               You should set J to the cost.
+    %               Compute the partial derivatives and set grad to the partial
+    %               derivatives of the cost w.r.t. each parameter in theta
+    % =============================================================
 
 
-J = getLogisticCost(theta, X, y) + cf_reg_expr;
+    %
+    % Cost function J(theta)
+    %
+    % As theta0 is not regularized, we skip it
+    cf_reg_expr = lambda/(2*m) * sum(theta([2 : size(theta)]) .^ 2);
 
-%
-% Gradients
-%
 
-% As theta0 is not regualrized, we do not apply lambda factor
-grad = gradient(theta, X, y);
-grad_one = grad(1); % Store theta0 value as it's not regularized.
+    J = getLogisticCost(theta, X, y) + cf_reg_expr;
 
-% Regularization expression for non theta0 values
-gd_reg_expr = lambda/m * theta;
+    %
+    % Gradients
+    %
 
-% Add regularization for all values of theta(weights), but we will replace
-% theta0 with the non regularized value we caluclated earlier.
+    % As theta0 is not regualrized, we do not apply lambda factor
+    grad = gradient(theta, X, y);
+    grad_one = grad(1); % Store theta0 value as it's not regularized.
 
-grad =  grad + gd_reg_expr;
+    % Regularization expression for non theta0 values
+    gd_reg_expr = lambda/m * theta;
 
-% As theta0 is not regularized, we replace it with the non regularized
-% value
-grad(1) = grad_one;
+    % Add regularization for all values of theta(weights), but we will replace
+    % theta0 with the non regularized value we caluclated earlier.
+
+    grad =  grad + gd_reg_expr;
+
+    % As theta0 is not regularized, we replace it with the non regularized
+    % value
+    grad(1) = grad_one;
+
 end
